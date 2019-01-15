3b4330d96129d45aac1ea7e2252ac364aa3db051
djx <cloveses@126.com>
2019-01-15 17:29:52 +0800

    m

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>rHello Flask</h1>"
+
+@app.route('/gettest')
+def gettest():
+    print(request.args.get('sname',''))
+    print(request.args['sname'])
+    return 'success!'
