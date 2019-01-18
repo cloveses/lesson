044047c6e761eef97f5039905ecba1f3eb09df7a
djx <cloveses@126.com>
2019-01-18 17:40:00 +0800

    m

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>rHello Flask</h1>"
+    return "<h1>Hello Flask</h1>"
 
-@app.route('/gettest')
+@app.route('/gtest')
 def gettest():
-    print(request.args.get('sname',''))
-    print(request.args['sname'])
+    print(request.args.get('name',''))
+    print(request.args['name'])
     return 'success!'
 
-@app.route('/posttest', methods=['GET','POST'])
+@app.route('/ptest', methods=['GET','POST'])
 def posttest():
     if request.method == 'GET':
         return '<form method="post"><input name="name" type="text"><input type="submit"></form>'
     elif request.method == 'POST':
         print(request.form['name'])
+        print(request.form.get('sname','defaults'))
         return 'aa'
+
+@app.errorhandler(404)
+def ff(error):
+    return 'err',404
