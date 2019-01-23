7f6a1c106b007a1d6f157f3f02e1d2ac87ea58cf
djx <1181564186@qq.com>
2019-01-23 20:58:09 +0800

    e

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask,request
+from flask import Flask ,request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Hello Flask</h1>"
 
+@app.route('/index/<float:uid>', methods=['GET','POST'])
+def indexp(uid):
+    if request.method == 'GET':
+        print(type(uid))
+        return 'hhh'
+    else:
+        return "<h1>Hello Flask</h1>"
+
 @app.route('/gtest')
 def gettest():
     print(request.args.get('name',''))
-    print(request.args['name'])
+    # try:
+    #     print(request.args['name'])
+    # except KeyError as e:
+    #     print('kkk')
+    print(request.url)
+    print(request.base_url)
     return 'success!'
 
 @app.route('/ptest', methods=['GET','POST'])
@@ -20,6 +33,6 @@ def posttest():
         print(request.form.get('sname','defaults'))
         return 'aa'
 
-@app.errorhandler(404)
-def ff(error):
-    return 'err',404
+# @app.errorhandler(404)
+# def ff(error):
+#     return 'err',404
